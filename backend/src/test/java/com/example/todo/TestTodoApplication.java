1ce7137e3977feec07d4eb2d3c87a1cb43af7860
qsunki <ajsthfldu@outlook.com>
2025-02-11 18:17:09 +0900

    Init project

---
+++ backend/src/test/java/com/example/todo/TestTodoApplication.java
@@ -0,0 +1,12 @@
+package com.example.todo;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(TodoApplication::main)
+                .with(TestcontainersConfiguration.class)
+                .run(args);
+    }
+}
