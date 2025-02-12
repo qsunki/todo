3f288fe705eda2439519ff39ac8256db3ea42bdd
qsunki <ajsthfldu@outlook.com>
2025-02-13 03:03:47 +0900

    Create Todo model

---
+++ backend/src/main/java/com/example/mytodo/todo/domain/Todo.java
@@ -0,0 +1,56 @@
+package com.example.mytodo.todo.domain;
+
+import java.time.LocalDateTime;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("todos")
+public class Todo {
+    @Id
+    private Long id;
+
+    private String content;
+    private boolean completed;
+    private LocalDateTime createdTime;
+    private LocalDateTime lastModifiedTime;
+    private Long userId;
+
+    public Todo(
+            Long id,
+            String content,
+            boolean completed,
+            LocalDateTime createdTime,
+            LocalDateTime lastModifiedTime,
+            Long userId) {
+        this.id = id;
+        this.content = content;
+        this.completed = completed;
+        this.createdTime = createdTime;
+        this.lastModifiedTime = lastModifiedTime;
+        this.userId = userId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public LocalDateTime getCreatedTime() {
+        return createdTime;
+    }
+
+    public LocalDateTime getLastModifiedTime() {
+        return lastModifiedTime;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+}
