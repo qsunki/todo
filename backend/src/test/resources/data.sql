07fa859c016f1f9452124d12077fe60c9c3fe3b7
qsunki <ajsthfldu@outlook.com>
2025-02-13 03:06:45 +0900

    Create test data

---
+++ backend/src/test/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO users (username, password)
+VALUES ('user1', '1234');
+
+INSERT INTO todos (content, completed, created_time, last_modified_time, user_id)
+VALUES ('Learn Spring Boot', true, '2024-02-13 10:00:00', '2024-02-13 10:00:00', 1),
+       ('Write SQL Scripts', true, '2024-02-13 11:00:00', '2024-02-13 11:30:00', 1),
+       ('Build a REST API', true, '2024-02-13 12:00:00', '2024-02-13 12:45:00', 1),
+       ('Explore Hibernate', false, '2024-02-13 13:00:00', '2024-02-13 13:15:00', 1),
+       ('Create a CRUD App', false, '2024-02-13 14:00:00', '2024-02-13 14:30:00', 1);
