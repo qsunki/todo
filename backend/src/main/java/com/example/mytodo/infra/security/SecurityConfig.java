f0049969d643435ff58fe29bf3c74cc875cb7ca6
qsunki <ajsthfldu@outlook.com>
2025-02-15 00:00:11 +0900

    Set up security configuration

--- backend/src/main/java/com/example/mytodo/infra/security/SecurityConfig.java
+++ backend/src/main/java/com/example/mytodo/infra/security/SecurityConfig.java
@@ -1,15 +1,64 @@
 package com.example.mytodo.infra.security;
 
+import com.example.mytodo.user.domain.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration(proxyBeanMethods = false)
 public class SecurityConfig {
 
+    private final ObjectMapper objectMapper;
+
+    public SecurityConfig(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationProvider restAuthenticationProvider)
+            throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.authenticationProvider(restAuthenticationProvider);
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+        http.authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.POST, "/api/users")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .addFilterBefore(
+                        restAuthenticationFilter(authenticationManager), UsernamePasswordAuthenticationFilter.class)
+                .authenticationManager(authenticationManager)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider restAuthenticationProvider(
+            UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return new RestAuthenticationProvider(userRepository, passwordEncoder);
+    }
+
+    private RestAuthenticationFilter restAuthenticationFilter(AuthenticationManager authenticationManager) {
+        RestAuthenticationFilter restAuthenticationFilter = new RestAuthenticationFilter(objectMapper);
+        restAuthenticationFilter.setAuthenticationManager(authenticationManager);
+        restAuthenticationFilter.setAuthenticationSuccessHandler(new RestAuthenticationSuccessHandler(objectMapper));
+        restAuthenticationFilter.setAuthenticationFailureHandler(new RestAuthenticationFailureHandler(objectMapper));
+        return restAuthenticationFilter;
     }
 }
