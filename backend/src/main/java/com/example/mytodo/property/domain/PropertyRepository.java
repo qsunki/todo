a3e12a53e7dcb1ab4de8058ef237ad4faa8c2502
qsunki <ajsthfldu@outlook.com>
2025-02-15 22:34:11 +0900

    Add Property repository

---
+++ backend/src/main/java/com/example/mytodo/property/domain/PropertyRepository.java
@@ -0,0 +1,5 @@
+package com.example.mytodo.property.domain;
+
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface PropertyRepository extends ListCrudRepository<Property, Long> {}
