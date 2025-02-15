4da6ae6906eaef7e42827d063f47c5b8d656f9c2
qsunki <ajsthfldu@outlook.com>
2025-02-15 22:44:47 +0900

    Add setter method for todoId in PropertyCreateReq

--- backend/src/main/java/com/example/mytodo/property/application/command/PropertyCreateReq.java
+++ backend/src/main/java/com/example/mytodo/property/application/command/PropertyCreateReq.java
@@ -3,4 +3,8 @@ package com.example.mytodo.property.application.command;
 import com.example.mytodo.property.domain.PropertyData;
 import com.example.mytodo.property.domain.PropertyType;
 
-public record PropertyCreateReq(Long todoId, String name, PropertyType type, PropertyData data) {}
+public record PropertyCreateReq(Long todoId, String name, PropertyType type, PropertyData data) {
+    public PropertyCreateReq setTodoId(Long todoId) {
+        return new PropertyCreateReq(todoId, name, type, data);
+    }
+}
