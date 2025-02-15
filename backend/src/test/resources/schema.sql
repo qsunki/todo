7d164fb3f69dceb942e933128aa9612e9fd06893
qsunki <ajsthfldu@outlook.com>
2025-02-15 22:28:14 +0900

    Reformat schema.sql

--- backend/src/test/resources/schema.sql
+++ backend/src/test/resources/schema.sql
@@ -8,11 +8,11 @@ CREATE TABLE IF NOT EXISTS users
 
 CREATE TABLE IF NOT EXISTS todos
 (
-    id      BIGINT AUTO_INCREMENT PRIMARY KEY,
-    content VARCHAR(255) NOT NULL,
-    completed BOOLEAN NOT NULL,
-    created_time TIMESTAMP NOT NULL,
-    last_modified_time TIMESTAMP NOT NULL,
-    user_id BIGINT       NULL,
+    id                 BIGINT AUTO_INCREMENT PRIMARY KEY,
+    content            VARCHAR(255) NOT NULL,
+    completed          BOOLEAN      NOT NULL,
+    created_time       TIMESTAMP    NOT NULL,
+    last_modified_time TIMESTAMP    NOT NULL,
+    user_id            BIGINT       NULL,
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
