c51ca1dc21179dff7a0ddadc016cb9a4d4ab3f13
qsunki <ajsthfldu@outlook.com>
2025-02-16 05:22:33 +0900

    Add properties test data

--- backend/src/test/resources/data.sql
+++ backend/src/test/resources/data.sql
@@ -7,3 +7,23 @@ VALUES ('Learn Spring Boot', true, '2024-02-13 10:00:00', '2024-02-13 10:00:00',
        ('Build a REST API', true, '2024-02-13 12:00:00', '2024-02-13 12:45:00', 1),
        ('Explore Hibernate', false, '2024-02-13 13:00:00', '2024-02-13 13:15:00', 1),
        ('Create a CRUD App', false, '2024-02-13 14:00:00', '2024-02-13 14:30:00', 1);
+
+INSERT INTO properties (name, type, data, todo_id)
+VALUES ('마감일', 'DATE', '{"type":"date", "start":"2025-02-10T00:00:00", "end":null}', 1),
+       ('우선순위', 'SELECT', '{"type":"select", "name":"상", "color":"red"}', 1);
+
+INSERT INTO properties (name, type, data, todo_id)
+VALUES ('마감일', 'DATE', '{"type":"date", "start":"2025-02-11T00:00:00", "end":null}', 2),
+       ('우선순위', 'SELECT', '{"type":"select", "name":"하", "color":"gray"}', 2);
+
+INSERT INTO properties (name, type, data, todo_id)
+VALUES ('마감일', 'DATE', '{"type":"date", "start":"2025-02-12T00:00:00", "end":null}', 3),
+       ('우선순위', 'SELECT', '{"type":"select", "name":"상", "color":"red"}', 3);
+
+INSERT INTO properties (name, type, data, todo_id)
+VALUES ('마감일', 'DATE', '{"type":"date", "start":"2025-02-13T00:00:00", "end":null}', 4),
+       ('우선순위', 'SELECT', '{"type":"select", "name":"하", "color":"gray"}', 4);
+
+INSERT INTO properties (name, type, data, todo_id)
+VALUES ('마감일', 'DATE', '{"type":"date", "start":"2025-02-14T00:00:00", "end":null}', 5),
+       ('우선순위', 'SELECT', '{"type":"select", "name":"중", "color":"blue"}', 5);
